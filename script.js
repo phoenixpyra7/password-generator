44524aabc84393c5774b3ea8832d7e4015e0303d
Seven <phoenixpyra7@gmail.com>
2023-11-04 12:31:50 -0500

    trying an if-else for the character set

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+/*added a function for a return of a confirmation the button was clicked*/
+function generatePassword() {
+  console.log("The generate passwerd button has been clicked")
+/*thinking that an if-else will help select character range, status was crossed out so I selected permission status instead*/
+if ((charaters >= 1) && (characters <= 28)) {
+  PermissionStatus = "Acceptable.";
+} else {
+  PermissionStatus = "Not accepptable.";
+}
+}
+// 
+
+
+
+  /*return that variable*/
+  return "Generated password will go here";
+}
 
 // Write password to the #password input
 function writePassword() {
