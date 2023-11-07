fab87859dcc3777d9a64cd0f4a2feabb1892bcd8
Seven <phoenixpyra7@gmail.com>
2023-11-06 18:08:39 -0600

    added some more notes to remind me what I did

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- var lowerCase = upperCase.toLocaleLowerCase(); //this will change the capitols to lower.
- var number = "123456789"
- var spcChar = "!@#$%&?~" //selected basic special chars.
+ var lowerCase = upperCase.toLocaleLowerCase(); //this will change the capitals to lower case.
+ var number = "123456789" //these are the numbers available to use.
+ var spcChar = "!@#$%&?~" //selected basic special chars to keep it simple.
  var result = ""; //global
  var possChars = ""; //global
  var guarantiedChar = "" //global
 
 
 function generatePassword() {
-/*added a function for a return to confirm that the button was clicked*/
-//console.log("The generate password button has been clicked")
+/*added a function for a return, to confirm that the button was clicked*/
+//console.log("The generate password button has been clicked")- removed this.
 //added that a length must be selected or it will not continue to next prompt.
   var userLength = parseInt(prompt("How many characters between 8 and 128 would you like for your password to be?"));
   //added an alert if anything but a number is typed in.
@@ -31,7 +31,7 @@ function generatePassword() {
   //Do I add 2 more of these? i cant get it to work for numb/spcchar
   /*if(spcChar(userLength)) {
     console.log(userLength)
-    alert("You must choose a number!");
+    alert("You must choose either a number or special character or both!");
     return null;
   }*/  
 //added prompt questions/ the confirm method used.
@@ -60,21 +60,19 @@ function generatePassword() {
     possChars += spcChar;
     guarantiedChar += spcChar[Math.floor(Math.random() * possChars.length)];
   }
-
-
-// do i type 0-7 or just leave it as 0?
+//i should say 0 not 0-7, for future reference.
 for (var i = 0; i < userLength; i++) {
   var random = Math.floor(Math.random() * possChars.length);
   result += possChars[random];
 
 console.log("RESULT: ", result) // i cant remember what this is for
 //var chars = "abcde";
-}
+} 
 //loop through gaurantiedChar done
 // console.log(result); // Theoretical output - "cae";   - I can't recall what we were discussing this in regard to
   /*return that variable*/
 return result;
-} // this white curly brace doesnt do anything but if deleted, page wont work????
+} // this white curly brace ends the entire section which started on line 13.
 // Write password to the #password input - cannot recall where this comes in when choosing algorithms/layout.
 function writePassword() {
   var password = generatePassword();
